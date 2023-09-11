9633215287270a3e1886a68529565e56a9809e1b
lajtosrob <lajtosrobert@freemail.hu>
2023-09-11 09:10:45 +0200

    Add project files.

---
+++ ConsoleApp_Versenyzok/Pilotak.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Versenyzok
+{
+    internal class Pilotak
+    {
+        string nev;
+        string szuletesDatuma;
+        string nemzetiseg;
+        string rajtszam;
+
+        public Pilotak(string nev, string szuletesDatuma, string nemzetiseg, string rajtszam)
+        {
+            this.nev = nev;
+            this.szuletesDatuma = szuletesDatuma;
+            this.nemzetiseg = nemzetiseg;
+            this.rajtszam = rajtszam;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string SzuletesDatuma { get => szuletesDatuma; set => szuletesDatuma = value; }
+        public string Nemzetiseg { get => nemzetiseg; set => nemzetiseg = value; }
+        public string Rajtszam { get => rajtszam; set => rajtszam = value; }
+    }
+}
---
+++ ConsoleApp_Versenyzok/Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+using balkezesek;
+using ConsoleApp_Versenyzok;
+using System.Linq;
+// See https://aka.ms/new-console-template for more information
+
+namespace balkezesek;
+class Program
+{
+
+    static List<Pilotak> PilotakList = new List<Pilotak>();
+
+    static void Main(string[] args)
+
+    {
+        //2. pilotak.csv sorainak beolvasása 
+
+        StreamReader sr = new StreamReader("DATAS/pilotak.csv");
+
+        string headerLine = sr.ReadLine();
+
+        while(!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(';');
+
+            Pilotak PilotakData = new Pilotak(
+                line[0],
+                line[1],
+                line[2],
+                line[3]
+                );
+
+            PilotakList.Add( PilotakData ); 
+
+        }
+
+        sr.Close();
+
+        // 3. 
+
+        Console.WriteLine($"3. feladat: {PilotakList.Count()}");
+
+        // 4. 
+
+        Console.WriteLine($"4. feladat: {PilotakList.Last().Nev}");
+
+        // 5. 
+
+        Console.WriteLine("5. feladat:");
+
+        List<Pilotak> PilotakXIX = PilotakList.Where(x => isPilotakXIX(x.SzuletesDatuma)).ToList();
+
+        foreach (Pilotak pilota in PilotakXIX)
+        {
+            Console.WriteLine($"\t{pilota.Nev} ({pilota.SzuletesDatuma})");
+        }
+
+        static bool isPilotakXIX(string szuletesiDatum)
+        {
+            if (DateTime.TryParse(szuletesiDatum, out DateTime datum) )
+            {
+                return datum.Year < 1901;
+            }
+            return false;
+        }
+
+        // 6. 
+
+        Console.Write("6. feladat: ");
+
+        Pilotak legkisebbRajtszamu = PilotakList
+            .Where(p => !string.IsNullOrEmpty(p.Rajtszam)) // A rajtszámmal nem rendelkező pilótákat nem vesszük figyelembe.
+            .OrderBy(p =>int.Parse(p.Rajtszam)) // Rendezés növekvő sorrendbe rajtszám alapján
+            .FirstOrDefault();  // Az első pilóta kiválasztása a listában. 
+
+        Console.Write(legkisebbRajtszamu.Nemzetiseg);
+        Console.WriteLine();
+
+        // 7.
+
+        Console.Write("7. feladat:");
+
+        var tobbRajtszam = PilotakList
+            .Where(p => !string.IsNullOrEmpty (p.Rajtszam))
+            .GroupBy(x => x.Rajtszam)
+            .Where(g => g.Count() > 1)
+            .Select(g => g.Key)
+            .ToList();
+
+        tobbRajtszam.ForEach(x => Console.Write($" {x}"));
+
+    }
+}
